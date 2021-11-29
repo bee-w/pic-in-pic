65d1541a20784e28c14bb8b0d0fa435e33e0a1f3
Brigette Maher <bv1232@outlook.com.au>
2021-11-29 17:07:27 +1100

    initial complete

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+
+// Prompt to select media stream
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        
+    }
+}
+
+startButton.addEventListener('click', async () => {
+    // Disable button
+    startButton.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    startButton.disabled = false;
+});
+
+stopButton.addEventListener('click', () => {
+    if (document.pictureInPictureElement) {
+        document
+            .exitPictureInPicture()
+            .catch(error => {
+
+            })
+    } else{
+
+    }
+});
+
+// On load
+selectMediaStream();
+
+// stopButton.disabled = true;
+// // Start picture in picture
+// await videoElement.exitPictureInPicture();
+// // Reset Button
+// stopButton.disabled = false;
